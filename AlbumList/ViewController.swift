4a4cf7ebf258998235450ce5bcc6eef41d84577e
Melisa Öztürk <mlsaoztrk@icloud.com>
2019-04-03 14:01:25 +0300

    Initial Commit

---
+++ AlbumList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlbumList
+//
+//  Created by melisa öztürk on 3.04.2019.
+//  Copyright © 2019 melisa öztürk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
